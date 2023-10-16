08dcf5cfbb0b575d7350a02d3e1731171128ff99
Baananou <bananoumohamed30@gmail.com>
2023-10-16 18:25:49 +0100

    project init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,13 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+	content: [
+		"./node_modules/flowbite/**/*.js",
+		"./node_modules/flowbite-react/**/*.js",
+		"./public/**/*.html",
+		"./app/**/*.{ts,tsx}",
+	],
+	plugins: [require("flowbite/plugin")],
+	theme: {},
+};
